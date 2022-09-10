6f462c289c20a7b6daa9a87d50779aa5a6fd798a
covidsolar <112794083+covidsolar@users.noreply.github.com>
2022-09-10 16:32:05 +0800

    Parallelize data parsing

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,12 +4,11 @@ using System.Text.RegularExpressions;
 
 IEnumerable<string> files = Directory.EnumerateFiles("../data_source/csse_covid_19_data/csse_covid_19_daily_reports", "*.csv");
 Regex dateMatcher = new Regex("(?<month>\\d{2})-(?<date>\\d{2})-(?<year>\\d{4})");
-
-foreach (var file in files)
-{
-    
+Dictionary<DateTime, List<SourceCovidData>> data = new Dictionary<DateTime, List<SourceCovidData>>();
+Parallel.ForEach(files,async file => {
     string fileName = Path.GetFileNameWithoutExtension(file);
-    List<SourceCovidData> records = ReadDataFromCsv(file);;
+    Console.WriteLine($"Parsing {fileName}");
+    List<SourceCovidData> records = await ReadDataFromCsv(file);;
     MatchCollection matches = dateMatcher.Matches(fileName);
     if (matches.Count == 1)
     {
@@ -18,20 +17,18 @@ foreach (var file in files)
         string year = matches[0].Groups["year"].Value;
         string dateStr = $"{year}-{month}-{date}";
         DateTime dateValue = DateTime.Parse(dateStr);
-        foreach (var record in records)
-        {
-            record.date = dateValue;
-        }
+        data.Add(dateValue, records);
     }
     else
     {
         Console.WriteLine($"Unable to parse date from file name {fileName}");
     }
     Console.WriteLine($"Successfully parsed {records.Count} records from {fileName}");
-}
+});
 
 
-List<SourceCovidData> ReadDataFromCsv(string path)
+
+async Task<List<SourceCovidData>> ReadDataFromCsv(string path)
 {
     var records = new List<SourceCovidData>();
     using (var reader = new StreamReader(path))
@@ -98,6 +95,4 @@ public class SourceCovidData
     public int recovered { get; set; }
     public int active { get; set; }
     public double incident_rate { get; set; }
-
-    public DateTime date { get; set; }
 }
